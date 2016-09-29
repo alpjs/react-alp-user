e368c9edd60b496109b17264d57fbbe43ddcc064
Christophe Hurpeau <christophe@hurpeau.com>
2016-09-30 01:53:32 +0200

    pob-babel@8.3.1

--- lib-node6/index.js
+++ lib-node6/index.js
@@ -5,13 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = UserComponent;
 
-var _tcombForked = require('tcomb-forked');
-
-var _tcombForked2 = _interopRequireDefault(_tcombForked);
-
 var _react = require('react');
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 UserComponent.propTypes = {
   children: _react.PropTypes.func.isRequired
@@ -21,42 +15,10 @@ UserComponent.contextTypes = {
   context: _react.PropTypes.object
 };
 
-const Props = _tcombForked2.default.interface({
-  children: _tcombForked2.default.Function
-}, 'Props');
-
 function UserComponent(_ref, _ref2) {
-  var _assert2 = _assert(_ref, Props, '{ children }');
-
-  let children = _assert2.children;
+  let children = _ref.children;
   let user = _ref2.context.state.user;
 
-  _assert({
-    children
-  }, Props, '{ children }');
-
   return children(user);
-}
-
-function _assert(x, type, name) {
-  function message() {
-    return 'Invalid value ' + _tcombForked2.default.stringify(x) + ' supplied to ' + name + ' (expected a ' + _tcombForked2.default.getTypeName(type) + ')';
-  }
-
-  if (_tcombForked2.default.isType(type)) {
-    if (!type.is(x)) {
-      type(x, [name + ': ' + _tcombForked2.default.getTypeName(type)]);
-
-      _tcombForked2.default.fail(message());
-    }
-
-    return type(x);
-  }
-
-  if (!(x instanceof type)) {
-    _tcombForked2.default.fail(message());
-  }
-
-  return x;
 }
 //# sourceMappingURL=index.js.map
--- lib-webpack-modern-browsers/index.js
+++ lib-webpack-modern-browsers/index.js
@@ -1,4 +1,3 @@
-import _t from 'tcomb-forked';
 import { PropTypes } from 'react';
 
 UserComponent.propTypes = {
@@ -9,40 +8,10 @@ UserComponent.contextTypes = {
   context: PropTypes.object
 };
 
-var Props = _t.interface({
-  children: _t.Function
-}, 'Props');
-
 export default function UserComponent(_ref, _ref2) {
   var children = _ref.children;
   var user = _ref2.context.state.user;
 
-  _assert({
-    children
-  }, Props, '{ children }');
-
   return children(user);
-}
-
-function _assert(x, type, name) {
-  function message() {
-    return 'Invalid value ' + _t.stringify(x) + ' supplied to ' + name + ' (expected a ' + _t.getTypeName(type) + ')';
-  }
-
-  if (_t.isType(type)) {
-    if (!type.is(x)) {
-      type(x, [name + ': ' + _t.getTypeName(type)]);
-
-      _t.fail(message());
-    }
-
-    return type(x);
-  }
-
-  if (!(x instanceof type)) {
-    _t.fail(message());
-  }
-
-  return x;
 }
 //# sourceMappingURL=index.js.map
